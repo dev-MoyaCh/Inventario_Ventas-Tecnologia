0413b61ed5e2b599542f5242c2fb3f40f3e7077c
dev-MoyaCh <71960460@continental.edu.pe>
2026-06-24 22:16:39 -0500

    Modulo Dashboard V.2.0

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,1 +1,40 @@
+<?php
 
+namespace Controllers;
+
+use Core\Controller;
+use Models\Producto;
+use Models\Venta;
+use Models\Ingreso;
+use Models\Salida;
+
+class DashboardController extends Controller
+{
+    private Producto $producto;
+    private Venta $venta;
+    private Ingreso $ingreso;
+    private Salida $salida;
+
+    public function __construct() {
+        $this->producto = new Producto();
+        $this->venta = new Venta();
+        $this->ingreso = new Ingreso();
+        $this->salida = new Salida();
+    }
+
+    public function index(): void {
+        $productos = $this->producto->getAll();
+
+        $stats = [
+            'productos'         => count($productos),
+            'stock_total'       => array_sum(array_column($productos, 'Stock')),
+            'valor_inventario'  => $this->producto->getStockValue(),
+            'total_ventas'      => $this->venta->getTotalVentas(),
+            'unidades_vendidas' => $this->venta->getTotalUnidades(),
+            'ingresos'          => $this->ingreso->getTotalUnidades(),
+            'salidas'           => $this->salida->getTotalUnidades(),
+        ];
+
+        $this->render('dashboard', ['stats' => $stats]);
+    }
+}
